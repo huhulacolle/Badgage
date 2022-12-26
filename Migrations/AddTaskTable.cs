d0439f86c802922d0024057cd18b7c17ca9f5641
sacha0914 <sacha0914.poidevin@gmail.com>
2022-12-26 16:16:08 +0100

    Task

---
+++ Migrations/AddTaskTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Badgage.Migrations
+{
+    [Migration(2)]
+    public class AddTaskTable : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Task");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Task")
+                .WithColumn("idtache").AsInt32().PrimaryKey().Identity()
+                .WithColumn("nomdetache").AsString().NotNullable().Unique()
+                .WithColumn("description").AsString().NotNullable().Unique()
+                .WithColumn("datefin").AsDateTime().NotNullable().Unique()
+                .WithColumn("datecreation").AsDateTime().NotNullable().Unique();
+        }
+    }
+}
