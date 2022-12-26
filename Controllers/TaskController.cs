022b9d1d91bdff4ceeb6a319f3162bf2b8f40c6e
sacha0914 <96061674+sacha0914@users.noreply.github.com>
2022-12-26 19:15:47 +0100

    Task (#15)

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Badgage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public async Task<ActionResult<Models.Task>> GetTask()
+        {
+            return Ok("No Task");
+        }
+
+
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace Badgage.Models
+{
+    public class Task
+    {
+    
+            public string TaskName { get; set; }
+            public string Descripton { get; set; }
+            public DateTime EndDate { get; set; }
+            public DateTime CreateDate { get; set; }
+        
+    }
+}
+
