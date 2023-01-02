42272afef9830efb70a4125c145c5450b5b8beb5
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-01-02 11:40:51 +0100

    migration session

---
+++ Migrations/AddSessionsTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Badgage.Migrations
+{
+    [Migration(9)]
+    public class AddSessionsTable : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Sessions")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("idTask").AsInt32().ForeignKey("Task", "idTask")
+                .WithColumn("idUser").AsInt32().ForeignKey("User", "idUtil")
+                .WithColumn("DateDebut").AsDateTime()
+                .WithColumn("DateFin").AsDateTime();
+        }
+    }
+}
