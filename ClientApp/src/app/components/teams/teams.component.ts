194bd9d93712bddb549f30408461dfc9f90f4eb1
Gianni <72388911+GianniB77@users.noreply.github.com>
2023-01-02 21:52:13 +0100

    fix snackbar

--- ClientApp/src/app/components/teams/teams.component.ts
+++ ClientApp/src/app/components/teams/teams.component.ts
@@ -37,10 +37,10 @@ export class TeamsComponent {
       team.byUser = new JwtHelperService().decodeToken(this.storageService.getUser().toString()).id;
       this.teamService.createTeam(team)
         .then(() => {
-          this._snackBar.open('Equipe créée');
+          this._snackBar.open('Equipe créée', '', {duration: 3000});
           this.getTeamByUser();
         }).catch(() => {
-          this._snackBar.open('Erreur lors de la création de l\'équipe');
+          this._snackBar.open('Erreur lors de la création de l\'équipe', '', {duration: 3000});
         });
     })
   }
@@ -52,10 +52,10 @@ export class TeamsComponent {
     dialogRef.afterClosed().subscribe((result) => {
       userOnTeam.idUser = result;
       this.teamService.joinTeam(userOnTeam).then(() => {
-        this._snackBar.open("Utilisateur ajouté avec succès dans l'équipe");
+        this._snackBar.open("Utilisateur ajouté avec succès dans l'équipe", '', {duration: 3000});
         this.getTeamByUser();
       }).catch(() => {
-        this._snackBar.open("Erreur lors de l'ajout de l'utilisateur dans l'équipe")
+        this._snackBar.open("Erreur lors de l'ajout de l'utilisateur dans l'équipe", '', {duration: 3000});
       })
     })
   }
