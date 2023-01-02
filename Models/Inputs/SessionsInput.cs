39116fe54d04981acb2277c506e18b1fe1bfe2cb
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-01-02 21:59:04 +0100

    session wihout idUser

--- Models/Inputs/SessionsInput.cs
+++ Models/Inputs/SessionsInput.cs
@@ -3,7 +3,6 @@
     public class SessionInput
     {
         public int IdTask { get; set; }
-        public int IdUser { get; set; }
         public DateTime DateDebut { get; set; }
         public DateTime? DateFin { get; set; }
     }
